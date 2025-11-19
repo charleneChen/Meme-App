e637efd2b6a29c76d96a93aa0bc9d3482917c558
Charlene.Chen <satikey@gmail.com>
2025-11-19 15:24:04 +1100

    add eventListener click to get image btn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
 
 emotionRadios.addEventListener("change", function (e) {
     const radios = document.getElementsByClassName("radio");
@@ -13,6 +14,17 @@ emotionRadios.addEventListener("change", function (e) {
         .getElementById(e.target.id)
         .parentElement.classList.add("highlight");
 });
+
+getImageBtn.addEventListener("click", getMatchingCatsArray);
+
+function getMatchingCatsArray() {
+    if (document.querySelector('input[type="radio"]:checked')) {
+        const selectedEmotion = document.querySelector(
+            'input[type="radio"]:checked'
+        ).value;
+        console.log(selectedEmotion);
+    }
+}
 
 function getEmotionsArray(cats) {
     const emotionsArray = [];
