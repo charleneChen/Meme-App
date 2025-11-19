ad57d143771c9b25dffa0763d6c3401b07308365
Charlene.Chen <satikey@gmail.com>
2025-11-19 12:03:27 +1100

    Update index.js with a horrible for loop

--- index.js
+++ index.js
@@ -168,3 +168,17 @@ const catsData = [
         alt: "A cat looking sad",
     },
 ];
+
+// A horrible for loop
+
+function getEmotionArray(cats) {
+    const emotionsArray = [];
+    for (let i = 0; i < cats.length; i++) {
+        for (let j = 0; j < cats[i].emotionTags.length; j++) {
+            emotionsArray.push(cats[i].emotionTags[j]);
+        }
+    }
+    console.log(emotionsArray);
+}
+
+getEmotionArray(catsData);
